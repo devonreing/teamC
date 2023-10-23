57f42dcd6b6a3bc11be438f740e6dd20de50c348
lmcginley620 <146997511+lmcginley620@users.noreply.github.com>
2023-10-23 10:25:45 -0400

    Update rommate_project.sql

--- rommate_project.sql
+++ rommate_project.sql
@@ -1,6 +1,6 @@
 use roommate_project;
 CREATE TABLE Users (
-    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    UserID INT(6) PRIMARY KEY,
     Username VARCHAR(255) NOT NULL,
     PasswordHash VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL,
@@ -10,10 +10,10 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE RoommatePreferences (
-    PreferenceID INT AUTO_INCREMENT PRIMARY KEY,
-    UserID INT,
-    MinAge INT,
-    MaxAge INT,
+    PreferenceID INT(6) PRIMARY KEY,
+    UserID INT(6),
+    MinAge INT(110),
+    MaxAge INT(110),
     GenderPreference ENUM('Male', 'Female', 'Any'),
     SmokingPreference ENUM('Non-smoker', 'Smoker', 'No preference'),
     PetPreference ENUM('No pets', 'Cats', 'Dogs', 'Other', 'No preference'),
@@ -21,17 +21,17 @@ CREATE TABLE RoommatePreferences (
 );
 
 CREATE TABLE LikesDislikes (
-    LikeDislikeID INT AUTO_INCREMENT PRIMARY KEY,
-    UserID INT,
+    LikeDislikeID INT (6) PRIMARY KEY,
+    UserID INT(6),
     Likes TEXT, 
     Dislike TEXT,
     FOREIGN KEY (UserID) REFERENCES Users (UserID)
 );
 
 CREATE TABLE Matches (
-    MatchID INT AUTO_INCREMENT PRIMARY KEY,
-    User1ID INT,
-    User2ID INT,
+    MatchID INT (6) PRIMARY KEY,
+    User1ID INT (6),
+    User2ID INT (6),
     MatchScore DECIMAL(5, 2),
     FOREIGN KEY (User1ID) REFERENCES Users (UserID),
     FOREIGN KEY (User2ID) REFERENCES Users (UserID)
